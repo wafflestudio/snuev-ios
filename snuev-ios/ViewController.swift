40b38446c62324e7351862939d08466859ceb98e
ydh1304 <donghyun.lee@mvlchain.io>
2019-01-08 21:39:39 +0900

    Initial Commit

---
+++ snuev-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snuev-ios
+//
+//  Created by 이동현 on 08/01/2019.
+//  Copyright © 2019 이동현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
