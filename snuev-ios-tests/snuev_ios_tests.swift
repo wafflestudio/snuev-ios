f7999b50cbb0b31ae30b656bb6bb5dcb9f43e664
ydh1304 <donghyun.lee@mvlchain.io>
2019-01-18 00:18:42 +0900

    add tests

---
+++ snuev-ios-tests/snuev_ios_tests.swift
@@ -0,0 +1,32 @@
+//
+//  snuev_ios_tests.swift
+//  snuev-ios-tests
+//
+//  Created by 이동현 on 18/01/2019.
+//  Copyright © 2019 이동현. All rights reserved.
+//
+
+import XCTest
+
+class snuev_ios_tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
