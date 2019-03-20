1c6fe6588dfa9deed8edb57553de8a0e60dc930d
ydh1304 <donghyun.lee@mvlchain.io>
2019-03-20 12:55:13 +0900

    temp commit

--- snuev-ios/Utils/PrimitiveSequence.swift
+++ snuev-ios/Utils/PrimitiveSequence.swift
@@ -13,8 +13,7 @@ import Japx
 
 extension PrimitiveSequence where PrimitiveSequence.TraitType == RxSwift.SingleTrait {
     func mapResponseToArray<T: Mappable>(_ type: T.Type) -> RxSwift.PrimitiveSequence<RxSwift.SingleTrait, [T]?> {
-        return self.subscribeOn(MainScheduler.instance)
-            .observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
+        return self
             .map { response in
                 guard let response = response as? Response else {
                     return nil
@@ -22,7 +21,10 @@ extension PrimitiveSequence where PrimitiveSequence.TraitType == RxSwift.SingleT
                 do {
                     let filteredResponse = try response.filterSuccessfulStatusCodes()
                     let decodedResponse = try Japx.Decoder.jsonObject(withJSONAPIObject: filteredResponse.mapJSON() as! Parameters)
-                    return Mapper<T>().mapArray(JSONArray: decodedResponse["data"] as! [[String : Any]])
+                    if let data = decodedResponse["data"] as? [[String: Any]] {
+                        return Mapper<T>().mapArray(JSONArray: data)
+                    }
+                    return []
                 } catch _ {
                     return nil
                 }
